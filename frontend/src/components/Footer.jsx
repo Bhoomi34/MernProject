1ec9b35f66ff675d97a10f39e565de144886eda2
Your Name <bhoomirgupta26@gmail.com>
2024-08-07 23:03:00 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">BHOOMI</div>
+          <div className="right">
+            <p>FoodyZone,Mumbai</p>
+            <p>Open: 06:00 PM - 11:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Bhoomi</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved by Bhoomi</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
